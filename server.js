4de2d7c8768209904854aca4118499921319df36
MartaKoss27 <27691497m@gmail.com>
2024-04-08 18:48:22 +0200

    mongoose and dotenv install

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import { app } from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
