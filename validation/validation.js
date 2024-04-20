188b426975da8b97c1ead65b22e5c147bef22e43
MartaKoss27 <27691497m@gmail.com>
2024-04-20 15:11:54 +0200

    get contacts fix
    get all contacts for logged in user only

--- validation/validation.js
+++ validation/validation.js
@@ -1,6 +1,8 @@
 import Joi from "joi";
 
 export const schema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
   password: Joi.string().min(5).required(),
 });
